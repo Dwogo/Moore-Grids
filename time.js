3a819112b92fb9713c9467765efac92a69a7acf5
Dwog <philliptdavies@gmail.com>
2021-05-25 12:44:02 +1200

    Added the changing welcome

--- time.js
+++ time.js
@@ -1,6 +1,14 @@
 'use strict'
 
-var morn = "Good Morning";
+var gday = document.getElementById('timeWelcome');
 var timeOf = new Date();
+var time = timeOf.getHours();
 
-document.getElementById('timeWelcome').innerHTML = timeOf;
+if (time >= 0 && time < 12) {
+    gday.innerText = "Good Morning";
+} else if (time >= 12 && time < 16) {
+    gday.innerText = "Good Afternoon";
+} else if (time >= 16 && time <= 23) {
+    gday.innerText = "Good Evening";
+}
+
